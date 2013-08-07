fa70dfd0ec9ef104681c05cc082b8c631eeb5a76
Nguyen Xuan Mui <nguyenxuanmui@gmail.com>
2013-08-07 22:56:26 +0700

    Add basic component bte

---
+++ administrator/components/com_bte/helpers/bte.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * @copyright	Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+/**
+ * Banners component helper.
+ *
+ * @package		Joomla.Administrator
+ * @subpackage	com_bte
+ * @since		1.6
+ */
+class BteHelper
+{
+	/**
+	 * Configure the Linkbar.
+	 *
+	 * @param	string	The name of the active view.
+	 *
+	 * @return	void
+	 * @since	1.6
+	 */
+	public static function addSubmenu($vName)
+	{
+		JSubMenuHelper::addEntry(
+			JText::_('COM_BTE_SUBMENU_BANNERS'),
+			'index.php?option=com_bte&view=links',
+			$vName == 'links'
+		);
+
+		JSubMenuHelper::addEntry(
+			JText::_('COM_BTE_SUBMENU_CRAWLER'),
+			'index.php?option=com_bte&view=clients',
+			$vName == 'clients'
+		);
+	}
+
+	/**
+	 * Gets a list of the actions that can be performed.
+	 *
+	 * @param	int		The category ID.
+	 *
+	 * @return	JObject
+	 * @since	1.6
+	 */
+	public static function getActions($categoryId = 0)
+	{
+		$user	= JFactory::getUser();
+		$result	= new JObject;
+
+		if (empty($categoryId)) {
+			$assetName = 'com_bte';
+			$level = 'component';
+		} else {
+			$assetName = 'com_bte.category.'.(int) $categoryId;
+			$level = 'category';
+		}
+
+		$actions = JAccess::getActions('com_bte', $level);
+
+		foreach ($actions as $action) {
+			$result->set($action->name,	$user->authorise($action->name, $assetName));
+		}
+
+		return $result;
+	}
+}
